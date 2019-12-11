7bcc72786e006978d7b6713be69c9cce885c9608
zhimi99 <52504869+zhimi99@users.noreply.github.com>
2019-12-11 12:12:49 -0500

    Add files via upload

---
+++ agregarUsuario.php
@@ -0,0 +1,127 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latihan MySQLi</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include("nav.php");?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Datos de los Usuarios&raquo; Agregar datos</h2>
+			<hr />
+
+			<?php
+			if(isset($_POST['add'])){
+				$cedula		     = mysqli_real_escape_string($con,(strip_tags($_POST["cedula"],ENT_QUOTES)));//Escanpando caracteres 
+				$nombre		     = mysqli_real_escape_string($con,(strip_tags($_POST["nombre"],ENT_QUOTES)));//Escanpando caracteres 
+				$direccion	 = mysqli_real_escape_string($con,(strip_tags($_POST["direccion"],ENT_QUOTES)));//Escanpando caracteres 
+				$telefono	 = mysqli_real_escape_string($con,(strip_tags($_POST["telefono"],ENT_QUOTES)));//Escanpando caracteres 
+				$correo	     = mysqli_real_escape_string($con,(strip_tags($_POST["correo"],ENT_QUOTES)));//Escanpando caracteres 
+				$clave		 = mysqli_real_escape_string($con,
+                (strip_tags($_POST["clave"],ENT_QUOTES)));//Escanpando caracteres 
+				$estado	 = mysqli_real_escape_string($con,
+                (strip_tags($_POST["estado"],ENT_QUOTES)));//Escanpando caracteres 
+				
+			
+
+				$cek = mysqli_query($con, "SELECT * FROM usuarios WHERE cedula='$cedula'");
+				if(mysqli_num_rows($cek) == 0){
+						$insert = mysqli_query($con, "INSERT INTO usuarios(cedula, nombre, direccion, telefono, correo, clave, estado)
+															VALUES('$cedula','$nombre', '$direccion', '$telefono', '$correo', '$clave', '$estado')") or die(mysqli_error());
+						if($insert){
+							echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Bien hecho! Los datos han sido guardados con éxito.</div>';
+						}else{
+							echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. No se pudo guardar los datos !</div>';
+						}
+					 
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. código exite!</div>';
+				}
+			}
+			?>
+
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Cédula</label>
+					<div class="col-sm-2">
+						<input type="text" name="cedula" class="form-control" placeholder="Cédula" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombre</label>
+					<div class="col-sm-4">
+						<input type="text" name="nombre" class="form-control" placeholder="Nombre" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Dirección</label>
+					<div class="col-sm-4">
+						<input type="text" name="direccion" class="form-control" placeholder="Dirección" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Teléfono</label>
+					<div class="col-sm-4">
+						<input type="text" name="telefono" class="input-group date form-control"  placeholder="Teléfono" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Correo</label>
+					<div class="col-sm-3">
+						<textarea name="correo" class="form-control" placeholder="Correo"></textarea>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Clave</label>
+					<div class="col-sm-3">
+						<input type="text" name="clave" class="form-control" placeholder="Clave" required>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Estado</label>
+					<div class="col-sm-3">
+						<select name="estado" class="form-control">
+							<option value=""> ----- </option>
+                           <option value="1">Cliente</option>
+							<option value="2">Administrador</option>
+							
+						</select>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="add" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	
+</body>
+</html>
